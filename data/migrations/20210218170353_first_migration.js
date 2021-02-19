cb413c9c757c4f5bec4e7f5a707a5b6ab5101693
willmondal <wbmondal@aol.com>
2021-02-18 17:44:21 -0800

    more added to knexfile

--- data/migrations/20210218170353_first_migration.js
+++ data/migrations/20210218170353_first_migration.js
@@ -1,6 +1,21 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema
+  .createTable("recipes", tbl => {
+      tbl.increments("recipe_id")
+      tbl.string("recipe_name")
+  })
+  .createTable("ingredients", tbl => {
+    tbl.increments("ingredient_id")
+    tbl.string("ingredient_name")
+  })
+  .createTable("quantities", tbl => {
+    tbl.increments("quantity_id")
+    tbl.integer("quantity_num")
+  })
+  .createTable("meal_instructions", tbl => {
+
+  })
 };
 
 exports.down = function(knex) {
